378438966823bdaec916a9488aec79761d1fb46e
Verba de Quibus <static.faded.cpp@gmail.com>
2023-08-04 01:22:58 -0400

    Add project files.

---
+++ rapid_updates/main.cpp
@@ -0,0 +1,304 @@
+/*
+ * Copyright 2023 FadedStatic
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <format>
+
+#include "scanner.hpp"
+
+
+struct addr_match
+{
+	std::string name;
+	std::uintptr_t addr;
+};
+
+std::vector<addr_match> get_offs(const process& a)
+{
+	std::vector<addr_match> return_vector;
+
+	const auto start_time = std::chrono::high_resolution_clock::now();
+
+	const auto reqcheck = util::get_prologue(a, scanner::string_scan(a, "about:blank")[0].loc);
+	const auto push_captures = util::get_prologue(a, scanner::string_scan(a, "too many captures")[0].loc);
+	const auto push_captures_calls = util::get_calls(a, push_captures);
+	const auto luaL_error = push_captures_calls[2].loc;
+	const auto luaL_error_calls = util::get_calls(a, luaL_error);
+	const auto str_find_aux = util::get_prologue(a, scanner::xref_scan(a, push_captures)[1].loc);
+	const auto luaD_throw = luaL_error_calls[luaL_error_calls.size() - 2].loc;
+	const auto lua_exception_ctor = util::get_calls(a, luaD_throw)[0].loc;
+	const auto luaL_where = luaL_error_calls[0].loc;
+	const auto lua_pushvfstring = luaL_error_calls[1].loc;
+	const auto luaC_step = luaL_error_calls[2].loc;
+	const auto luaC_step_calls = util::get_calls(a, luaC_step);
+	const auto luaV_tostring = luaL_error_calls[3].loc;
+	const auto add_value = util::get_prologue(a, scanner::xref_scan(a, push_captures)[2].loc);
+	const auto add_value_xrefs = scanner::xref_scan(a, add_value);
+	const auto str_gsub = util::get_prologue(a, add_value_xrefs[0].loc);
+	const auto str_gsub_calls = util::get_calls(a, str_gsub);
+	const auto tag_error = util::get_calls(a, str_gsub)[10].loc;
+	const auto lua_typename = scanner::string_scan(a, "no value")[0].loc;
+	const auto luaL_typeerrorL = util::get_prologue(a, util::get_calls(a, tag_error)[1].loc);
+	const auto luaL_typeerrorL_calls = util::get_calls(a, luaL_typeerrorL);
+	const auto currfuncname = luaL_typeerrorL_calls[0].loc;
+	const auto luaA_toobject = luaL_typeerrorL_calls[1].loc;
+	const auto luaA_toobject_xrefs = scanner::xref_scan(a, luaA_toobject);
+	const auto luaL_typename = luaA_toobject_xrefs[7].loc;
+	const auto lua_breakpoint = luaA_toobject_xrefs[9].loc;
+	const auto lua_getcoverage = luaA_toobject_xrefs[10].loc;
+	const auto luaL_where_calls = util::get_calls(a, luaL_where);
+	const auto luaO_chunkid = luaL_where_calls[1].loc;
+	const auto luaT_objtypename = luaL_typeerrorL_calls[2].loc;
+	const auto luaT_objtypename_xrefs = scanner::xref_scan(a, luaT_objtypename);
+	const auto luaT_objtypenamestr = util::get_calls(a, luaT_objtypename)[0].loc;
+	const auto luaG_typeerrorL = util::get_prologue(a, luaT_objtypename_xrefs[1].loc);
+	const auto luaG_ordererror = util::get_prologue(a, luaT_objtypename_xrefs[5].loc);
+	const auto luaG_runerrorL = util::get_calls(a, luaG_ordererror)[2].loc;
+	const auto luaG_runerrorL_calls = util::get_calls(a, luaG_runerrorL);
+	const auto pusherror = luaG_runerrorL_calls[1].loc;
+	const auto pusherror_calls = util::get_calls(a, pusherror);
+	const auto luaO_chunkid_xrefs = scanner::xref_scan(a, luaO_chunkid);
+	const auto luau_load_inlined = util::get_prologue(a, luaO_chunkid_xrefs[3].loc);
+	const auto getfunc = util::get_prologue(a, luaO_chunkid_xrefs[21].loc);
+	const auto currentline = pusherror_calls[1].loc;
+	const auto currentline_xrefs = scanner::xref_scan(a, currentline);
+	const auto luaL_argerrorL = util::get_prologue(a, currentline_xrefs[0].loc);
+	const auto luaO_pushfstring = pusherror_calls[2].loc;
+	const auto luaO_pushfstring_calls = util::get_calls(a, luaO_pushfstring);
+	const auto luaO_pushvfstring = luaO_pushfstring_calls[0].loc;
+	const auto luaO_pushvfstring_calls = util::get_calls(a, luaO_pushvfstring);
+	const auto luaO_pushvfstring_xrefs = scanner::xref_scan(a, luaO_pushvfstring);
+	const auto lua_pushfstringL = util::get_prologue(a, luaO_pushvfstring_xrefs[1].loc);
+	const auto lua_pushfstringL_xrefs = scanner::xref_scan(a, lua_pushfstringL);
+	const auto luaL_tolstring = util::get_prologue(a, lua_pushfstringL_xrefs[8].loc);
+	const auto auxresume = util::get_prologue(a, lua_pushfstringL_xrefs[9].loc);
+	const auto auxresume_calls = util::get_calls(a, auxresume);
+	const auto lua_resume = auxresume_calls[3].loc;
+	const auto lua_resume_calls = util::get_calls(a, lua_resume);
+	const auto resume_error = lua_resume_calls[0].loc;
+	const auto lua_xmove = auxresume_calls[4].loc;
+	const auto lua_xpush = util::get_prologue(a, lua_xmove + 0x10);
+	const auto luaD_rawrunprotected = lua_resume_calls[2].loc;
+	const auto lua_resumeerror = util::get_prologue(a, scanner::xref_scan(a, resume_error)[2].loc);
+	const auto lua_resumeerror_calls = util::get_calls(a, lua_resumeerror);
+	const auto resume_findhandler = lua_resumeerror_calls[1].loc;
+	const auto resume_finish = lua_resumeerror_calls[3].loc;
+	const auto seterrorobj = util::get_calls(a, resume_finish)[0].loc;
+	const auto seterrorobj_xrefs = scanner::xref_scan(a, seterrorobj);
+	const auto seterrorobj_calls = util::get_calls(a, seterrorobj);
+	const auto newgcoblock = seterrorobj_calls[2].loc;
+	const auto luaB_pcally = util::get_prologue(a, seterrorobj_xrefs[2].loc);
+	const auto luaB_pcally_calls = util::get_calls(a, luaB_pcally);
+	const auto lua_rawcheckstack = luaB_pcally_calls[4].loc;
+	const auto lua_rawcheckstack_xrefs = scanner::xref_scan(a, lua_rawcheckstack);
+	const auto db_info = util::get_prologue(a, lua_rawcheckstack_xrefs[0].loc);
+	const auto luaB_pcallcont = util::get_prologue(a, lua_rawcheckstack_xrefs[2].loc);
+	const auto luaB_xpcallcont = util::get_prologue(a, lua_rawcheckstack_xrefs[4].loc);
+	const auto auxresumecont = util::get_prologue(a, lua_rawcheckstack_xrefs[6].loc);
+	const auto luaL_pushresults = str_gsub_calls[9].loc;
+	const auto luaL_pushresults_xrefs = scanner::xref_scan(a, luaL_pushresults);
+	const auto codepoint = util::get_prologue(a, scanner::string_scan(a, "string slice too long")[0].loc);
+	const auto codepoint_calls = util::get_calls(a, codepoint);
+	const auto lua_tointegerx = codepoint_calls[3].loc;
+	const auto lua_tointegerx_calls = util::get_calls(a, lua_tointegerx);
+	const auto pseudo2addr = lua_tointegerx_calls[0].loc;
+	const auto lua_tounsignedx = lua_tointegerx_calls[1].loc;
+	const auto luaL_checkstack = codepoint_calls[4].loc;
+	const auto lua_checkstack = util::get_calls(a, luaL_checkstack)[0].loc;
+	const auto utf8_decode = codepoint_calls[5].loc;
+	const auto utf8_decode_xrefs = scanner::xref_scan(a, utf8_decode);
+	const auto utflen = util::get_prologue(a, utf8_decode_xrefs[0].loc);
+	const auto iter_aux = util::get_prologue(a, utf8_decode_xrefs[2].loc);
+	const auto db_traceback = util::get_prologue(a, luaL_pushresults_xrefs[0].loc);
+	const auto utfchar = util::get_prologue(a, luaL_pushresults_xrefs[1].loc);
+	const auto utfchar_calls = util::get_calls(a, utfchar);
+	const auto buffutfchar = utfchar_calls[0].loc;
+	const auto str_format = util::get_prologue(a, luaL_pushresults_xrefs[8].loc);
+	const auto str_format_calls = util::get_calls(a, str_format);
+	const auto luaL_checklstring = str_format_calls[0].loc;
+	const auto luaL_extendbuffer = str_format_calls[1].loc;
+	const auto luaL_extendbuffer_calls = util::get_calls(a, luaL_extendbuffer);
+	const auto luaM_toobig = luaL_extendbuffer_calls[1].loc;
+	const auto newpage = luaL_extendbuffer_calls[1].loc;
+	const auto newgcoblock2 = luaL_extendbuffer_calls[1].loc;
+	const auto luaL_checknumber = str_format_calls[4].loc;
+	const auto lua_clock = luaC_step_calls[0].loc;
+	const auto gcstep = luaC_step_calls[3].loc;
+	const auto gcstep_calls = util::get_calls(a, gcstep);
+	const auto gcstep_xrefs = scanner::xref_scan(a, gcstep);
+	const auto luaC_fullgc = util::get_prologue(a, gcstep_xrefs[1].loc);
+	const auto luaC_fullgc_calls = util::get_calls(a, luaC_fullgc);
+	const auto luaC_fullgc_xrefs = scanner::xref_scan(a, luaC_fullgc);
+	const auto markroot = gcstep_calls[0].loc;
+	const auto markroot_calls = util::get_calls(a, markroot);
+	const auto propagatemark = gcstep_calls[1].loc;
+	const auto propagatemark_calls = util::get_calls(a, propagatemark);
+	const auto reallymarkobject = markroot_calls[0].loc;
+	const auto finishGcCycleMetrics = luaC_fullgc_calls[2].loc;
+	const auto shrinkbuffersfull = luaC_fullgc_calls[6].loc;
+	const auto lua_collectgarbage = util::get_prologue(a, luaC_fullgc_xrefs[0].loc);
+	const auto resume_handle = util::get_prologue(a, seterrorobj_xrefs[0].loc);
+	const auto resume_handle_calls = util::get_calls(a, resume_handle);
+	const auto luau_poscall = resume_handle_calls[2].loc;
+	const auto luau_poscall_xrefs = scanner::xref_scan(a, luau_poscall);
+	const auto resume_continue = util::get_prologue(a, luau_poscall_xrefs[0].loc);
+	const auto resume_continue_xrefs = scanner::xref_scan(a, resume_continue);
+	const auto resume = util::get_prologue(a, resume_continue_xrefs[1].loc);
+	const auto resume_calls = util::get_calls(a, resume);
+	const auto luaV_tryfuncTM = resume_calls[0].loc;
+	const auto metatable_str_xrefs = scanner::string_scan(a, "__metatable");
+	const auto luaB_getmetatable = metatable_str_xrefs[1].loc;
+	const auto luaB_setmetatable = metatable_str_xrefs[2].loc;
+	const auto luaB_setmetatable_calls = util::get_calls(a, luaB_setmetatable);
+	const auto luaL_getmetafield = luaB_setmetatable_calls[0].loc;
+	const auto luaC_barrierf = luaB_setmetatable_calls[1].loc;
+	const auto luaC_barrierf_xrefs = scanner::xref_scan(a, luaC_barrierf);
+	const auto lua_replace = luaC_barrierf_xrefs[7].loc;
+	const auto gmatch_aux = luaC_barrierf_xrefs[11].loc;
+	const auto clearupvals = luaC_barrierf_xrefs[13].loc;
+	const auto luau_execute = luaC_barrierf_xrefs[17].loc;
+	const auto luau_execute_singlestep = luaC_barrierf_xrefs[14].loc;
+	const auto tfreeze = metatable_str_xrefs[3].loc;
+	const auto tclone = metatable_str_xrefs[4].loc;
+	const auto lua_isnumber = util::get_calls(a, util::get_prologue(a, scanner::string_scan(a, "attempt to multiply a Vector2 with an incompatible value type or nil")[0].loc))[1].loc;
+
+
+	const auto end_time = std::chrono::high_resolution_clock::now();
+	std::printf("Time taken: %lldms\r\n", std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count());
+
+	return_vector.push_back({ "reqcheck", reqcheck });
+	return_vector.push_back({ "push_captures", push_captures });
+	return_vector.push_back({ "luaL_error", luaL_error });
+	return_vector.push_back({ "str_find_aux", str_find_aux });
+	return_vector.push_back({ "luaD_throw", luaD_throw });
+	return_vector.push_back({ "luaL_where", luaL_where });
+	return_vector.push_back({ "lua_pushvfstring", lua_pushvfstring });
+	return_vector.push_back({ "luaC_step", luaC_step });
+	return_vector.push_back({ "luaV_tostring", luaV_tostring });
+	return_vector.push_back({ "add_value", add_value });
+	return_vector.push_back({ "str_gsub", str_gsub });
+	return_vector.push_back({ "tag_error", tag_error });
+	return_vector.push_back({ "lua_typename", lua_typename });
+	return_vector.push_back({ "luaL_typeerrorL", luaL_typeerrorL });
+	return_vector.push_back({ "currfuncname", currfuncname });
+	return_vector.push_back({ "luaO_chunkid", luaO_chunkid });
+	return_vector.push_back({ "luaT_objtypename", luaT_objtypename });
+	return_vector.push_back({ "luaT_objtypenamestr", luaT_objtypenamestr });
+	return_vector.push_back({ "luaG_typeerrorL", luaG_typeerrorL });
+	return_vector.push_back({ "luaG_ordererror", luaG_ordererror });
+	return_vector.push_back({ "luaG_runerrorL", luaG_runerrorL });
+	return_vector.push_back({ "pusherror", pusherror });
+	return_vector.push_back({ "luau_load_inlined", luau_load_inlined });
+	return_vector.push_back({ "getfunc", getfunc });
+	return_vector.push_back({ "currentline", currentline });
+	return_vector.push_back({ "luaL_argerrorL", luaL_argerrorL });
+	return_vector.push_back({ "luaO_pushfstring", luaO_pushfstring });
+	return_vector.push_back({ "luaO_pushvfstring", luaO_pushvfstring });
+	return_vector.push_back({ "lua_pushfstringL", lua_pushfstringL });
+	return_vector.push_back({ "luaL_tolstring", luaL_tolstring });
+	return_vector.push_back({ "auxresume", auxresume });
+	return_vector.push_back({ "lua_resume", lua_resume });
+	return_vector.push_back({ "resume_error", resume_error });
+	return_vector.push_back({ "lua_xmove", lua_xmove });
+	return_vector.push_back({ "lua_xpush", lua_xpush });
+	return_vector.push_back({ "luaD_rawrunprotected", luaD_rawrunprotected });
+	return_vector.push_back({ "lua_resumeerror", lua_resumeerror });
+	return_vector.push_back({ "resume_finish", resume_finish });
+	return_vector.push_back({ "seterrorobj", seterrorobj });
+	return_vector.push_back({ "newgcoblock", newgcoblock });
+	return_vector.push_back({ "resume_findhandler", resume_findhandler });
+	return_vector.push_back({ "luaB_pcally", luaB_pcally });
+	return_vector.push_back({ "lua_rawcheckstack", lua_rawcheckstack });
+	return_vector.push_back({ "db_info", db_info });
+	return_vector.push_back({ "luaB_pcallcont", luaB_pcallcont });
+	return_vector.push_back({ "luaB_xpcallcont", luaB_xpcallcont });
+	return_vector.push_back({ "auxresumecont", auxresumecont });
+	return_vector.push_back({ "luaL_pushresults", luaL_pushresults });
+	return_vector.push_back({ "codepoint", codepoint });
+	return_vector.push_back({ "lua_tointegerx", lua_tointegerx });
+	return_vector.push_back({ "lua_tounsignedx", lua_tounsignedx });
+	return_vector.push_back({ "pseudo2addr", pseudo2addr });
+	return_vector.push_back({ "luaL_checkstack", luaL_checkstack });
+	return_vector.push_back({ "utf8_decode", utf8_decode });
+	return_vector.push_back({ "utflen", utflen });
+	return_vector.push_back({ "iter_aux", iter_aux });
+	return_vector.push_back({ "db_traceback", db_traceback });
+	return_vector.push_back({ "utfchar", utfchar });
+	return_vector.push_back({ "buffutfchar", buffutfchar });
+	return_vector.push_back({ "str_format", str_format });
+	return_vector.push_back({ "luaL_checklstring", luaL_checklstring });
+	return_vector.push_back({ "luaL_extendbuffer", luaL_extendbuffer });
+	return_vector.push_back({ "luaM_toobig", luaM_toobig });
+	return_vector.push_back({ "newpage", newpage });
+	return_vector.push_back({ "newgcoblock2", newgcoblock2 });
+	return_vector.push_back({ "luaL_checknumber", luaL_checknumber });
+	return_vector.push_back({ "lua_clock", lua_clock });
+	return_vector.push_back({ "gcstep", gcstep });
+	return_vector.push_back({ "luaC_fullgc", luaC_fullgc });
+	return_vector.push_back({ "markroot", markroot });
+	return_vector.push_back({ "propagatemark", propagatemark });
+	return_vector.push_back({ "reallymarkobject", reallymarkobject });
+	return_vector.push_back({ "finishGcCycleMetrics", finishGcCycleMetrics });
+	return_vector.push_back({ "shrinkbuffersfull", shrinkbuffersfull });
+	return_vector.push_back({ "lua_collectgarbage", lua_collectgarbage });
+	return_vector.push_back({ "resume_handle", resume_handle });
+	return_vector.push_back({ "luau_poscall", luau_poscall });
+	return_vector.push_back({ "resume_continue", resume_continue });
+	return_vector.push_back({ "resume", resume });
+	return_vector.push_back({ "lua_exception::lua_exception", lua_exception_ctor });
+	return_vector.push_back({ "luaV_tryfuncTM", luaV_tryfuncTM });
+	return_vector.push_back({ "luaA_toobject", luaA_toobject });
+	return_vector.push_back({ "lua_getcoverage", lua_getcoverage });
+	return_vector.push_back({ "lua_breakpoint", lua_breakpoint });
+	return_vector.push_back({ "luaL_typename", luaL_typename });
+	return_vector.push_back({ "lua_checkstack", lua_checkstack });
+	return_vector.push_back({ "luaB_getmetatable", luaB_getmetatable });
+	return_vector.push_back({ "luaB_setmetatable", luaB_setmetatable });
+	return_vector.push_back({ "tfreeze", tfreeze });
+	return_vector.push_back({ "tclone", tclone });
+	return_vector.push_back({ "luaL_getmetafield", luaL_getmetafield });
+	return_vector.push_back({ "luaC_barrierf", luaC_barrierf });
+	return_vector.push_back({ "lua_replace", lua_replace });
+	return_vector.push_back({ "gmatch_aux", gmatch_aux });
+	return_vector.push_back({ "clearupvals", clearupvals });
+	return_vector.push_back({ "luau_execute", luau_execute });
+	return_vector.push_back({ "luau_execute_singlestep", luau_execute_singlestep });
+	return_vector.push_back({ "lua_isnumber", lua_isnumber });
+
+	return return_vector;
+}
+
+
+
+int main()
+{
+	const scan_cfg cfg =
+	{
+		//"ntdll.dll"
+	};
+
+	const auto a = process("Windows10Universal.exe");
+	for (const auto d : get_offs(a))
+	{
+		std::printf("MakeName(0x%02llX, \"", util::rebase(a, d.addr), d.name);
+		std::cout << d.name << "\");\r\n";
+	}
+}
---
+++ rapid_updates/scanner.hpp
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2023 FadedStatic
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <functional>
+#include <iostream>
+#include <Windows.h>
+#include <shared_mutex>
+#include <string_view>
+#include <Psapi.h>
+#include <processthreadsapi.h>
+#include <thread>
+
+ // This part will be using preprocessor macros so that performance profiling isnt compiled by default, this way we save a negligible amount of CPU time
+#define PERFORMANCE_PROFILING_MODE false // true = true, false = false
+// False by default, this will set the priority of the process for better use of resources (faster scanning), this is advised for external applications but not for internal, as it could be detected easily.
+// Also note, this will set the priority back to the old priority when we are done with scanning.
+#define SET_PRIORITY_OPTIMIZATION true 
+
+
+// Misc. Settings for the 
+constexpr auto max_modules = 512; // 512 by default
+constexpr auto max_processes = 1024; // 1024 by default
+
+struct scan_result
+{
+	std::uintptr_t loc;
+};
+
+// Struct for process, this will just have like the pid and the PROCESS
+struct process
+{
+	std::uintptr_t pid, proc_base;
+	bool is32; // False means x86_64, True means just x86 
+	HANDLE curr_proc{ nullptr };
+	HMODULE curr_mod{ nullptr };
+	explicit process(const std::string_view process_name);
+};
+
+// INSTRUCTIONS FOR ADDING CUSTOM ARGUMENTS!
+// This struct will be UNTOUCHED by the scan function, and it is up to you to add whatever relevant information is required to this.
+// Now, when you add an argument or something it's not that important, you WILL have to compile the source as any linked libraries will contain a different definition of scanner_args, but no additional modifications will have to be made.
+// If you are to add an argument, please do it at the bottom-most variable, as it will not break any structured binding in the methods.
+struct scanner_optargs
+{
+	// This is where the optional args go in.
+	const std::string_view xref_trace_endianized; // This is the offset we are looking for in scanning, it's endianized.
+	const std::uintptr_t xref_trace_int;
+	// ADD PAST THIS LINE!
+};
+
+// Expr suggested making this a struct, so I did just that.
+struct scanner_args
+{
+	const process& proc;
+	const std::uintptr_t start;
+	const std::uintptr_t end;
+	std::shared_mutex& return_vector_mutex;
+	std::vector<scan_result>& return_vector;
+	const std::string_view aob;
+	const std::string_view mask;
+	scanner_optargs opt_args;
+};
+
+namespace scanner_cfg_templates
+{
+	// This is an example, you can either make this a function or a variable containing lambda or std::function.
+	// Argument must be std::uintptr_t and return must be bool.
+	const auto page_flag_check_default = [](const std::uintptr_t page_flags) -> bool
+	{
+		// Credits to Fishy for this genius optimization (see credits in readme for more information)
+		return !(page_flags & (PAGE_NOACCESS | PAGE_GUARD)) && (page_flags & (PAGE_READWRITE | PAGE_READONLY | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_READ)) && !(page_flags == PAGE_EXECUTE && !(page_flags & (PAGE_GUARD | PAGE_NOACCESS)));
+	};
+
+	// Reference the cpp file for more information about this.
+	void aob_scan_routine_external_default(const scanner_args& args),
+		string_xref_scan_external_default(const scanner_args& args),
+		function_xref_scan_external_default(const scanner_args& args);
+}
+
+// Struct for configuring scans.
+struct scan_cfg
+{
+	// Are we scanning a specific module?
+	std::string module_scanned;
+
+	// This is the page flag check, so if you want to filter it by access flags you can do so.
+	std::function<bool(const std::uintptr_t)> page_flag_check = scanner_cfg_templates::page_flag_check_default;
+
+	// Minimum and Maximum page size (in bytes)
+	std::uintptr_t min_page_size = 0ull, max_page_size = ~0ull; // credits to Fishy for the suggestion using bit not instead of 0xFFFFFFFFFFFFFFF
+
+	// Change these dependent on the scan function.
+	// By default, this is AOB scanning.
+	// This is more useful if you need to scan for something more specific (opaque predicates etc)
+	std::function<void(const scanner_args&) > scan_routine_external = scanner_cfg_templates::aob_scan_routine_external_default;
+};
+
+namespace scanner
+{
+	// The format should be "\xED\xEF\x0E", "??x"
+	std::vector<scan_result> scan(const process& proc, const std::string_view aob, const std::string_view mask, const scan_cfg& config = {}, const scanner_optargs& opt_args = {});
+
+	// BELOW THIS LINE YOU CANNOT CHANGE scan_routine_internal and scan_routine_external, doesnt matter if you do we ignore it anyways.
+
+	// This is for scanning string xrefs, so if you had a string such as "we are mcdonalds gaming", it would get all xrefs of that string.
+	// Null terminator is not important, if you are missing it it will find it anyways.
+	// n_result is the number result of the scan we are targeting.
+	std::vector<scan_result> string_scan(const process& proc, const std::string_view str, const scan_cfg& config = {}, const std::uintptr_t n_result = 0);
+
+	// WARNING:
+	// scanning include_twobyte can make it VERY SLOW!
+	// it IS NOT ADVISED that you set this to true.
+	std::vector<scan_result> xref_scan(const process& proc, const std::uintptr_t func, const scan_cfg& config = {}, const bool include_twobyte = false);
+}
+
+namespace util
+{
+	template <typename T> requires std::is_integral_v<T>
+	T rebase(const process& proc, const T address, const T new_base = 0)
+	{
+		return address - static_cast<T>(proc.proc_base) + new_base;
+	}
+
+	std::vector<scan_result> get_calls(const process& proc, const std::uintptr_t func);
+
+	// functions_only basically means that the JMPs do not count branches, only functions. if you're expecting only jmps to functions then use this, otherwise if scanning for branches do not use functions_only
+	// include_twobyte_jmps is whether or not you want to include jumps such as: JNE Jb
+	std::vector<scan_result> get_jumps(const process& proc, const std::uintptr_t func, const bool functions_only = true, const bool include_twobyte_jmps = false);
+
+	std::uintptr_t get_prologue(const process& proc, const std::uintptr_t func);
+
+	// all_alignment pretty much means that all bytes after ret must be 0x90 or 0xC3 until the next prologue in order for a match to occur.
+	// min_alignment is the amount of alignment that is required for a match to occur, so if you know that your epilogue will have x amount of alignment bytes, then this helps increase speed tremendously.
+	std::uintptr_t get_epilogue(const process& proc, const std::uintptr_t func, const bool all_alignment = true, const std::uint32_t min_alignment = 0);
+}
